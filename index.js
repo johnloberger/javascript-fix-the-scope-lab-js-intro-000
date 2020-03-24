e604faa0120f32bf601ad683a7b7543a793c8cba
John Loberger <johnloberger@gmail.com>
2020-03-24 01:30:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ function myAnimal() {
 }
 
 function yourAnimal() {
+<<<<<<< HEAD
   var animal = 'cat'
+=======
+  var animal = "cat"
+>>>>>>> 12069931a9c065c5079991714e126f01817078b6
   return animal
 }
 
 function add2(n) {
   const two = 2
+<<<<<<< HEAD
   return parseInt(n, 10) + two
+=======
+  return n + two
+>>>>>>> 12069931a9c065c5079991714e126f01817078b6
 }
 
 var funkyFunction = function() {
@@ -21,7 +29,11 @@ var funkyFunction = function() {
   }
 }
 
+<<<<<<< HEAD
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction()
 theFunk = theFunk()
+=======
+var theFunk = funkyFunction()()
+>>>>>>> 12069931a9c065c5079991714e126f01817078b6
